92ffb610a7ae15f1ce8315d1cf644326e679f60d
gunasekaran-code <gunasekaran.code@gmail.com>
2026-04-29 13:05:02 +0530

    Chnage the server MySQL to PostgreSQL

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -6,8 +6,8 @@ import 'package:image_picker/image_picker.dart';
 
 class ApiService {
 
-  // static const String baseUrl = 'https://e-commerce-app-spee.onrender.com/api';  // Render URL - Use this for deployed backend
-  static const String baseUrl = 'http://127.0.0.1:8000/api';     // localhost chrome (Flutter web)
+  static const String baseUrl = 'https://e-commerce-app-spee.onrender.com/api';  // Render URL - Use this for deployed backend
+  // static const String baseUrl = 'http://127.0.0.1:8000/api';     // localhost chrome (Flutter web)
 
   // static const String baseUrl = 'http://10.0.2.2:8000/api';        // mobile emulator (Android Studio)
   // static const String baseUrl = 'http://192.168.1.11/api';         // wifi network 
